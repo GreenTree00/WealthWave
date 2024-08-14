b5a134c061712d188d4992ba244a0a5e5a91a203
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-08-14 12:48:26 -0500

    adding usestate hooks to add and subtract

--- src/components/AddIncome.jsx
+++ src/components/AddIncome.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 function AddIncome () {
 
-
+const [formData, setFormData] = useState({
+    month: "", year: "", totalincome: "", sidehustleincome: "", stockincome: "", other: ""
+})
     
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+
+    const handleClick = async (event) => {
+        event.preventDefault()
+        console.log(formData);
+    }
 
     return (
         <div className="addIncome">
             <p>Add Income</p>
                 <form>
-                <label for="Income">Which Month of Income:</label>
-                <select id="Income" name="Income">
+                <label for="month">Which Month of Income:</label>
+                <select id="month" name="month" value={formData.month} onChange={handleChange}>
                 <option value="January">January</option>
                 <option value="Febuary">Febuary</option>
                 <option value="March">March</option>
@@ -25,7 +39,7 @@ function AddIncome () {
                 <option value="December">December</option>
                 </select>
                 <label for="year">Year:</label>
-                <select id="year" name="year">
+                <select id="year" name="year" value={formData.year} onChange={handleChange}>
                 <option value="2020">2020</option>
                 <option value="2021">2021</option>
                 <option value="2022">2022</option>
@@ -41,15 +55,15 @@ function AddIncome () {
                 </select>
                 <br />
                 <label for="totalincome">Total Income:</label><br />
-                <input type="text" id="totalincome" name="totalincome" /><br />
-                <label for="sideHustleIncome">Side Hustle Income:</label><br />
-                <input type="text" id="sideHustleIncome" name="sideHustleIncome"></input><br />
-                <label for="stockIncome">Stock Income:</label><br />
-                <input type="text" id="stockIncome" name="stockIncome"></input><br />
+                <input type="text" id="totalincome" name="totalincome" value={formData.totalincome} onChange={handleChange}/><br />
+                <label for="sidehustleincome">Side Hustle Income:</label><br />
+                <input type="text" id="sidehustleincome" name="sidehustleincome" value={formData.sidehustleincome} onChange={handleChange}></input><br />
+                <label for="stockincome">Stock Income:</label><br />
+                <input type="text" id="stockincome" name="stockincome" value={formData.stockincome} onChange={handleChange}></input><br />
                 <label for="other">Other:</label><br />
-                <input type="text" id="other" name="other"></input><br />
+                <input type="text" id="other" name="other" value={formData.other} onChange={handleChange}></input ><br />
                 <br />
-                <input type="submit" />
+                <input type="submit" onClick={handleClick}/>
                 </form>
         </div>
     )
--- src/components/DeductIncome.jsx
+++ src/components/DeductIncome.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 function DeductIncome () {
+
+        const [formData, setFormData] = useState({
+            month: "", year: "", totalexpense: "", housing: "", food: "", transportation: "", insurance: "", other: ""
+        })
+            
+            const handleChange = (event) => {
+                const {name, value} = event.target;
+                setFormData({
+                    ...formData,
+                    [name]: value
+                });
+            };
+        
+            const handleClick = async (event) => {
+                event.preventDefault()
+                console.log(formData);
+            }
+
     return (
         <div className="addExpense">
             <p>Add Expense</p>
             <form>
                 <label for="month">Which Month of Expense:</label>
-                <select id="month" name="month">
+                <select id="month" name="month" value={formData.month} onChange={handleChange}>
                 <option value="January">January</option>
                 <option value="Febuary">Febuary</option>
                 <option value="March">March</option>
@@ -21,7 +39,7 @@ function DeductIncome () {
                 <option value="December">December</option>
                 </select>
                 <label for="year">Year:</label>
-                <select id="year" name="year">
+                <select id="year" name="year" value={formData.year} onChange={handleChange}>
                 <option value="2020">2020</option>
                 <option value="2021">2021</option>
                 <option value="2022">2022</option>
@@ -36,20 +54,20 @@ function DeductIncome () {
                 <option value="2031">2031</option>
                 </select>
                 <br />
-                <label for="totalExpense">Total Expense:</label><br />
-                <input type="text" id="totalExpense" name="totalExpense" /><br />
+                <label for="totalexpense">Total Expense:</label><br />
+                <input type="text" id="totalexpensee" name="totalexpense" value={formData.totalexpense} onChange={handleChange}/><br />
                 <label for="housing">Housing:</label><br />
-                <input type="text" id="housing" name="housing"></input><br />
+                <input type="text" id="housing" name="housing" value={formData.housing} onChange={handleChange}></input><br />
                 <label for="food">Food:</label><br />
-                <input type="text" id="food" name="food"></input><br />
+                <input type="text" id="food" name="food" value={formData.food} onChange={handleChange}></input><br />
                 <label for="transportation">Transportation:</label><br />
-                <input type="text" id="transportation" name="transportation"></input><br />
+                <input type="text" id="transportation" name="transportation" value={formData.transportation} onChange={handleChange}></input><br />
                 <label for="insurance">Insurance:</label><br />
-                <input type="text" id="insurance" name="insurance"></input><br />
+                <input type="text" id="insurance" name="insurance" value={formData.insurance} onChange={handleChange}></input><br />
                 <label for="other">Other:</label><br />
-                <input type="text" id="other" name="other"></input><br />
+                <input type="text" id="other" name="other" value={formData.other} onChange={handleChange}></input><br />
                 <br />
-                <input type="submit" />
+                <input type="submit" onClick={handleClick}/>
                 </form>
         </div>
     )
