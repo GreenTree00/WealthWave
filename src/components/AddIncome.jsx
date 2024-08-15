01816e1245c161771f571621a8255bd3d12eef8d
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-08-14 21:50:50 -0500

    changing to input-tpye date

--- src/components/AddIncome.jsx
+++ src/components/AddIncome.jsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 function AddIncome () {
 
 const [formData, setFormData] = useState({
-    month: "", year: "", totalincome: "", sidehustleincome: "", stockincome: "", other: ""
+    date: "", totalincome: "", sidehustleincome: "", stockincome: "", other: ""
 })
     
     const handleChange = (event) => {
@@ -22,37 +22,9 @@ const [formData, setFormData] = useState({
     return (
         <div className="addIncome">
             <p>Add Income</p>
-                <form>
-                <label for="month">Which Month of Income:</label>
-                <select id="month" name="month" value={formData.month} onChange={handleChange}>
-                <option value="January">January</option>
-                <option value="Febuary">Febuary</option>
-                <option value="March">March</option>
-                <option value="April">April</option>
-                <option value="May">May</option>
-                <option value="June">June</option>
-                <option value="July">July</option>
-                <option value="August">August</option>
-                <option value="September">September</option>
-                <option value="October">October</option>
-                <option value="November">November</option>
-                <option value="December">December</option>
-                </select>
-                <label for="year">Year:</label>
-                <select id="year" name="year" value={formData.year} onChange={handleChange}>
-                <option value="2020">2020</option>
-                <option value="2021">2021</option>
-                <option value="2022">2022</option>
-                <option value="2023">2023</option>
-                <option value="2024">2024</option>
-                <option value="2025">2025</option>
-                <option value="2026">2026</option>
-                <option value="2027">2027</option>
-                <option value="2028">2028</option>
-                <option value="2029">2029</option>
-                <option value="2030">2030</option>
-                <option value="2031">2031</option>
-                </select>
+            <form>
+                <label for="birthday">Date:</label>
+                <input type="date" id="date" name="date" value={formData.date} onChange={handleChange}/>
                 <br />
                 <label for="totalincome">Total Income:</label><br />
                 <input type="text" id="totalincome" name="totalincome" value={formData.totalincome} onChange={handleChange}/><br />
