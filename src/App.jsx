e4d21adb059be7eb63e3a16b5dea512b07f5ee24
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-08-31 18:44:59 -0500

    change name of PieGraphDateFilter to BarGraphDateFilter

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 import AddIncome from "./components/AddIncome";
 import DeductIncome from "./components/DeductIncome";
 import PieGraph from "./components/PieGraph";
-import PieGraphDateFilter from "./components/PieGraphDateFilter";
+import BarGraphDateFilter from "./components/PieGraphDateFilter";
 import "./App.css"
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
     <Header />
     <div className="addData">
     <div className="grid">
-      <PieGraphDateFilter className="cell"/>
+      <BarGraphDateFilter className="cell"/>
       <PieGraph className="cell" />
       <AddIncome className="cell" />
       <DeductIncome className="cell" />
