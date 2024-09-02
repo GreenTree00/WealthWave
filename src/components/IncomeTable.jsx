51d3428da039f953b9b52c6ad377b4d72b7b573c
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-02 17:51:00 -0500

    create IncomeTable for BarGraphDateFilter

---
+++ src/components/IncomeTable.jsx
@@ -0,0 +1,41 @@
+function IncomeTable () {
+    return (
+        <table class="table is-striped is-fullwidth">
+  <thead>
+    <tr>
+      <th>Date</th>
+      <th>Job Income</th>
+      <th>Side Hustle Income</th>
+      <th>Stock Income</th>
+      <th>Other</th>
+      <th>Total Income</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>2024-08-31</td>
+      <td>Salary</td>
+      <td>Income</td>
+      <td>$3,000.00</td>
+      <td>Income</td>
+    </tr>
+    <tr>
+      <td>2024-08-30</td>
+      <td>Groceries</td>
+      <td>Food</td>
+      <td>-$150.00</td>
+      <td>Expense</td>
+    </tr>
+    <tr>
+      <td>2024-08-28</td>
+      <td>Rent</td>
+      <td>Housing</td>
+      <td>-$1,200.00</td>
+      <td>Expense</td>
+    </tr>
+    
+  </tbody>
+</table>
+    )
+}
+export default IncomeTable;
