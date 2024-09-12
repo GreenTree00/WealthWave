29a78b7c6a60d4612bce4a63fb10c80135354256
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-12 17:21:22 -0500

    add useState and buttons to make app only show one component at a time

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,39 @@
-import React from "react";
+import {useState} from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import AddIncome from "./components/AddIncome";
-import DeductIncome from "./components/DeductIncome";
-import PieGraph from "./components/PieGraph";
-import BarGraphDateFilter from "./components/BarGraphDateFilter";
+import BodyContent from "./components/BodyContent";
+import Pages from "./components/Pages";
 import "./App.css"
 
 function App() {
- 
+  
+  const [page, setPage] = useState("home");
+
+  function changePage(title) {
+    setPage(title);
+  }
 
   return (
     <>
-    <div className="container is-fluid">
-    <Header />
-    <div className="addData">
-    <div className="grid">
-      <BarGraphDateFilter className="cell"/>
-      <PieGraph className="cell" />
-      <AddIncome className="cell" />
-      <DeductIncome className="cell" />
-    </div>
-    </div>
-    </div>
-    <Footer />
+      <div className="container is-fluid">
+        <Header />
+        <Pages>
+        <button onClick={() => changePage("home")} className="navbar-item">Home</button>
+        <button onClick={() => changePage("bar")} className="navbar-item">Look Up By Day</button>
+        <button onClick={() => changePage("income")} className="navbar-item">Income</button>
+        <button onClick={() => changePage("expense")} className="navbar-item">Expense</button>
+        </Pages>
+        <br />
+        <br />
+        <div className="addData">
+          <div className="grid">
+            <BodyContent page={page} />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App
