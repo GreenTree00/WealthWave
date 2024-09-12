1baec03db12772d0e3327bce460d3e312990acb8
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-12 18:32:33 -0500

    remove unused code from header and moved it to buttons in app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
+
 
 function Header () {
+
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
 
@@ -9,38 +11,7 @@ function Header () {
         <p className="subtitle">Your Personal Finance Tracker</p>
       </div>
 
-  <div id="navbarBasicExample" className="navbar-menu">
-    <div className="navbar-start">
-      <a className="navbar-item">
-        Home
-      </a>
-
-      <a className="navbar-item">
-        Documentation
-      </a>
-
-      <div className="navbar-item has-dropdown is-hoverable">
-        <a className="navbar-link">
-          More
-        </a>
-
-        <div className="navbar-dropdown">
-          <a className="navbar-item">
-            About
-          </a>
-          <a className="navbar-item is-selected">
-            Jobs
-          </a>
-          <a className="navbar-item">
-            Contact
-          </a>
-          <hr className="navbar-divider" />
-          <a className="navbar-item">
-            Report an issue
-          </a>
-        </div>
-      </div>
-    </div>
+  
 
     <div className="navbar-end">
       <div className="navbar-item">
@@ -55,7 +26,7 @@ function Header () {
         </div>
       </div>
     </div>
-  </div>
+  
 </nav>
     )
 }
