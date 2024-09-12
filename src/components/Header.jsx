0049bf46dc7d6773436ee667589e6bdd262429ab
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-11 20:25:37 -0500

    add extra menu tabs to header to connect to more parts of project and start process on building login functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,61 @@ import React from 'react'
 
 function Header () {
     return (
-      <section className="hero">
-      <div className="hero-body">
+      <nav className="navbar" role="navigation" aria-label="main navigation">
+
+<div className="hero-body">
         <p className="title">WealthWave</p>
         <p className="subtitle">Your Personal Finance Tracker</p>
       </div>
-    </section>
+
+  <div id="navbarBasicExample" className="navbar-menu">
+    <div className="navbar-start">
+      <a className="navbar-item">
+        Home
+      </a>
+
+      <a className="navbar-item">
+        Documentation
+      </a>
+
+      <div className="navbar-item has-dropdown is-hoverable">
+        <a className="navbar-link">
+          More
+        </a>
+
+        <div className="navbar-dropdown">
+          <a className="navbar-item">
+            About
+          </a>
+          <a className="navbar-item is-selected">
+            Jobs
+          </a>
+          <a className="navbar-item">
+            Contact
+          </a>
+          <hr className="navbar-divider" />
+          <a className="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div className="navbar-end">
+      <div className="navbar-item">
+        <p>Username</p>
+        <div className="buttons">
+          <a className="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a className="button is-light">
+            Log in
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
     )
 }
 
