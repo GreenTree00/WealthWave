d61378c5c38c0048a0b85abb7c554a75cd1c2054
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-12 17:43:00 -0500

    create button parent component that will house the buttons as children

---
+++ src/components/Pages.jsx
@@ -0,0 +1,9 @@
+export default function Pages ({children}) {
+    return (
+<div id="navbarBasicExample" className="navbar-menu">
+    <div className="navbar-start center-content ">
+      {children}
+    </div>
+    </div>
+    )
+}
