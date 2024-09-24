d05244b9b163f1cf3a459e39e8ce972fd6b70e70
GreenTree00 <156139223+GreenTree00@users.noreply.github.com>
2024-09-24 11:48:11 -0500

    change useState default status, and setFunction for formData

--- src/components/EditData.jsx
+++ src/components/EditData.jsx
@@ -3,7 +3,7 @@ import FormInput from "./FormInput";
 
 export default function EditData ({typeofData, id}) {
 
-  const [formData, setFormData] = useState([{}]);
+  const [formData, setFormData] = useState({});
 
     function handleEdit (event) {
       const {name, value} = event.target;
@@ -12,7 +12,6 @@ export default function EditData ({typeofData, id}) {
         [name]: value
       }));
     }
-
 
     useEffect(() => {
       if (typeofData && id) {
@@ -28,7 +27,7 @@ export default function EditData ({typeofData, id}) {
             }
             const data = await response.json();
             const fetchedData = typeofData === 'Edit Income' ? data.resInc : data.resExp;
-            setFormData(fetchedData);
+            setFormData(fetchedData[0]);
           } catch (error) {
             console.error('Error fetching data:', error);
           }
@@ -54,19 +53,19 @@ export default function EditData ({typeofData, id}) {
         console.error(`Error updating your ${type}:`, error);
       }
     }
-   
+
     return  (
       <>
       {typeofData=="Edit Income"?
       <div>
             <p className="title">{typeofData}</p>
             <form className="box custom-box">
-                <FormInput For={"date"} Type={"date"} ID={"date"} Name={"date"} Value={formData[0].date ? formData[0].date.slice(0, 10) : ''} OnChange={handleEdit}>Date:</FormInput>
-                <FormInput For={"jobincome"} Type={"text"} ID={"job_income"} Name={"job_income"} Value={formData[0].job_income} OnChange={handleEdit}>Job Income:</FormInput>
-                <FormInput For={"sidehustleincome"} Type={"text"} ID={"side_hustle_income"} Name={"side_hustle_income"} Value={formData[0].side_hustle_income} OnChange={handleEdit}>Side Hustle Income:</FormInput>
-                <FormInput For={"stockincome"} Type={"text"} ID={"stock_income"} Name={"stock_income"} Value={formData[0].stock_income} OnChange={handleEdit}>Stock Income:</FormInput>
-                <FormInput For={"other"} Type={"text"} ID={"other"} Name={"other"} Value={formData[0].other} OnChange={handleEdit}>Other:</FormInput>
-                <FormInput For={"totalincome"} Type={"text"} ID={"total_income"} Name={"total_income"} Value={formData[0].total_income} OnChange={handleEdit}>Total Income:</FormInput>
+                <FormInput For={"date"} Type={"date"} ID={"date"} Name={"date"} Value={formData.date ? formData.date.slice(0, 10) : ''} OnChange={handleEdit}>Date:</FormInput>
+                <FormInput For={"jobincome"} Type={"text"} ID={"job_income"} Name={"job_income"} Value={formData.job_income} OnChange={handleEdit}>Job Income:</FormInput>
+                <FormInput For={"sidehustleincome"} Type={"text"} ID={"side_hustle_income"} Name={"side_hustle_income"} Value={formData.side_hustle_income} OnChange={handleEdit}>Side Hustle Income:</FormInput>
+                <FormInput For={"stockincome"} Type={"text"} ID={"stock_income"} Name={"stock_income"} Value={formData.stock_income} OnChange={handleEdit}>Stock Income:</FormInput>
+                <FormInput For={"other"} Type={"text"} ID={"other"} Name={"other"} Value={formData.other} OnChange={handleEdit}>Other:</FormInput>
+                <FormInput For={"totalincome"} Type={"text"} ID={"total_income"} Name={"total_income"} Value={formData.total_income} OnChange={handleEdit}>Total Income:</FormInput>
                 <input className="button is-primary" type="submit" onClick={() => submitEdit("Income")}/>
                 </form>
         </div>
@@ -75,14 +74,14 @@ export default function EditData ({typeofData, id}) {
         <div>
                 <p className="title">{typeofData}</p>
                 <form className="box custom-box">
-                <FormInput For={"date"} Type={"date"} ID={"date"} Name={"date"} Value={formData[0].date ? formData[0].date.slice(0, 10) : ''} OnChange={handleEdit}>Date:</FormInput>
-                <FormInput For={"housing"} Type={"text"} ID={"housing"} Name={"housing"} Value={formData[0].housing} OnChange={handleEdit}>Housing:</FormInput>
-                <FormInput For={"food"} Type={"text"} ID={"food"} Name={"food"} Value={formData[0].food} OnChange={handleEdit}>Food:</FormInput>
-                <FormInput For={"transportation"} Type={"text"} ID={"transportation"} Name={"transportation"} Value={formData[0].transportation} OnChange={handleEdit}>Transportation:</FormInput>
-                <FormInput For={"insurance"} Type={"text"} ID={"insurance"} Name={"insurance"} Value={formData[0].insurance} OnChange={handleEdit}>Insurance:</FormInput>
-                <FormInput For={"entertainment"} Type={"text"} ID={"text"} Name={"entertainment"} Value={formData[0].entertainment} OnChange={handleEdit}>Entertainment:</FormInput>
-                <FormInput For={"other"} Type={"text"} ID={"text"} Name={"other"} Value={formData[0].other} OnChange={handleEdit}>Other:</FormInput>
-                <FormInput For={"totalexpense"} Type={"text"} ID={"total_expense"} Name={"total_expense"} Value={formData[0].total_expense} OnChange={handleEdit}>Total Expense:</FormInput>
+                <FormInput For={"date"} Type={"date"} ID={"date"} Name={"date"} Value={formData.date ? formData.date.slice(0, 10) : ''} OnChange={handleEdit}>Date:</FormInput>
+                <FormInput For={"housing"} Type={"text"} ID={"housing"} Name={"housing"} Value={formData.housing} OnChange={handleEdit}>Housing:</FormInput>
+                <FormInput For={"food"} Type={"text"} ID={"food"} Name={"food"} Value={formData.food} OnChange={handleEdit}>Food:</FormInput>
+                <FormInput For={"transportation"} Type={"text"} ID={"transportation"} Name={"transportation"} Value={formData.transportation} OnChange={handleEdit}>Transportation:</FormInput>
+                <FormInput For={"insurance"} Type={"text"} ID={"insurance"} Name={"insurance"} Value={formData.insurance} OnChange={handleEdit}>Insurance:</FormInput>
+                <FormInput For={"entertainment"} Type={"text"} ID={"text"} Name={"entertainment"} Value={formData.entertainment} OnChange={handleEdit}>Entertainment:</FormInput>
+                <FormInput For={"other"} Type={"text"} ID={"text"} Name={"other"} Value={formData.other} OnChange={handleEdit}>Other:</FormInput>
+                <FormInput For={"totalexpense"} Type={"text"} ID={"total_expense"} Name={"total_expense"} Value={formData.total_expense} OnChange={handleEdit}>Total Expense:</FormInput>
                 <input className="button is-primary" type="submit" onClick={() => submitEdit("Expense")}/>
                 </form>
           </div>
